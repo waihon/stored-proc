128174b808a1b04987f946f00b89ef8b4750b8f2
Waihon Yew <yewwaihon@gmail.com>
2022-03-01 22:19:57 +0800

    Add a text parameter named product_name

--- uspFindProducts.sql
+++ uspFindProducts.sql
@@ -1,7 +1,14 @@
-/* EXEC uspFindProducts 900, 1000; */
+/***
+    EXECUTE uspFindProducts
+        @min_list_price = 900,
+        @max_list_price = 1000,
+        @name = 'Trek';
+*/
 ALTER PRECEDURE uspFindProducts(
     @min_list_price AS DECIMAL,
-    @max_list_price AS DECIMAL)
+    @max_list_price AS DECIMAL,
+    @name AS VARCHAR(MAX)
+)
 AS
 BEGIN
     SELECT
@@ -12,7 +19,8 @@ BEGIN
 
     WHERE
         list_price >= @min_list_price AND
-        list_price <= @max_list_price
+        list_price <= @max_list_price AND
+        product_name LIKE '%' + @name + '%'
     ORDER BY
         list_price;
 END;
