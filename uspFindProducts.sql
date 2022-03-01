06b5ad838e0ec10332961182f4db34198948d921
Waihon Yew <yewwaihon@gmail.com>
2022-03-01 23:13:25 +0800

    Future proof max_list_price by using NULL as its default value

--- uspFindProducts.sql
+++ uspFindProducts.sql
@@ -1,6 +1,9 @@
 /***
     @min_list_price and @max_list_price are parameters with default
     values and therefore are optional.
+    Using NULL instead of 999999 as the default value for @max_list_price
+    would cater for possible increase of maximum list price in the future.
+
     EXECUTE uspFindProducts
         @min_list_price = 6000,
         @name = 'Trek';
@@ -20,7 +23,7 @@ BEGIN
 
     WHERE
         list_price >= @min_list_price AND
-        list_price <= @max_list_price AND
+        (list_price IS NULL OR list_price <= @max_list_price) AND
         product_name LIKE '%' + @name + '%'
     ORDER BY
         list_price;
