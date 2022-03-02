4915e6a2631608a2c5aab29ee0c1df8aba5fbdcc
Waihon Yew <yewwaihon@gmail.com>
2022-03-02 14:53:51 +0800

    Calculate total sales by order year

---
+++ uspSalesByOrderYear.sql
@@ -0,0 +1,22 @@
+CREATE PROCEDURE uspSalesByOrderYear(
+    @order_year INT
+)
+AS
+BEGIN
+    DECLARE @sales INT;
+
+    SELECT
+        @sales = SUM(list_price * quantity)
+    FROM
+        sales.order_items i
+        INNER JOIN sales.orders o ON o.order_id = i.order_id
+    WHERE
+        YEAR(order_date) = @order_year;
+
+    SELECT @sales;
+
+    IF @sales > 1000000
+    BEGIN
+        PRINT 'Great! The sales amount in ' + @order_year + ' is greater than 1,000,000';
+    END;
+END;
