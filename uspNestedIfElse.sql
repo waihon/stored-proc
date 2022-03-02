310327137662739d5d5a552042e422460046e9be
Waihon Yew <yewwaihon@gmail.com>
2022-03-02 15:00:16 +0800

    Do nested  IF...ELSE

---
+++ uspNestedIfElse.sql
@@ -0,0 +1,21 @@
+CREATE PROCEDURE uspNestedIfElse
+AS
+BEGIN
+    DECLARE @x INT = 10,
+            @y INT = 20;
+
+    IF (@x > 10)
+    BEGIN
+        IF (@x < @y)
+            PRINT 'x > 0 and x < y';
+        ELSE
+            PRINT 'x > 0 and x >= y';
+    END
+    ELSE
+    BEGIN
+        IF (@x < @y)
+            PRINT 'x <= 0 and x < y';
+        ELSE
+            PRINT 'x <= 0 and x >= y';
+    END;
+END;
